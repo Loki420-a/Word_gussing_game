17be8a50c284089127b5a85de19f93efdaf6f99b
Loki420-a <sahil1705103@gmail.com>
2025-01-16 15:01:31 +0530

    Logic Implementation

--- main.py
+++ main.py
@@ -18,3 +18,28 @@ while not condition:
    
    user_guess = input("** A letter gussed by the user to be in game.**--\n").lower()
    display =""
+
+   for letter in computer_value:
+      if letter == user_guess:
+         display += letter
+         correct_letter.append(letter)
+
+      elif letter in correct_letter:
+         display +=letter
+
+      else:
+         display += '_'
+    
+
+   print("display:",display)
+
+   if user_guess not in computer_value:
+        print("You have guessed wrong letter. Lives will be reduced\n")
+        lives -= 1
+        if lives == 0:
+            print(f"You loose. Correct word is {computer_value}")
+            condition = True
+
+   if '_' not in display:
+      condition= True
+      print("Yo Won!")
